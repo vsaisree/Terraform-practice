71d7e66b4a6e0cab39a91bb3caa437e2acb650e4
saisree <vsaisree4u4r@gmail.com>
2023-11-18 16:57:39 +0000

    data change

--- variables.tf
+++ variables.tf
@@ -5,3 +5,17 @@ variable "AWS_REGION" {
 variable "ECS_INSTANCE_TYPE" {
   default = "t2.micro"
 }
+
+variable "ECS_AMIS" {
+  type = map(string)
+
+  default = {
+
+    us-east-1 = "ami-1924770e"
+
+    us-west-2 = "ami-56ed4936"
+
+    eu-west-1 = "ami-c8337dbb"
+
+  }
+}
